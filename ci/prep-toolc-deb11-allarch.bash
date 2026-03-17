82c5c6e8f37c91e499c23806c80ce67121de01e0
abdulazizumarovich <abdulazizumarovich@gmail.com>
2026-03-17 14:30:16 +0500

    Update script to use WORKDIR for GCC installation. build-linux-aarch64

--- ci/prep-toolc-deb11-allarch.bash
+++ ci/prep-toolc-deb11-allarch.bash
@@ -6,37 +6,39 @@ sudo apt-get -o Acquire::ForceIPv4=true update
 sudo apt-get -o Acquire::ForceIPv4=true install g++ make wget curl bzip2 autogen dejagnu ccache -y --quiet
 [[ $? -eq 0 ]] || exit 1
 
+WORKDIR=$(pwd)/gcc
+mkdir -p $WORKDIR
 
-if [[ ! -d ../3rd/gcc-${PICK_GCC_REL} ]]
+if [[ ! -d $WORKDIR/gcc-${PICK_GCC_REL} ]]
 then
     USE_GNU_MIRROR="ftp.gnu.org"
     [[ -n $PICK_GNU_MIRROR ]] && USE_GNU_MIRROR=$PICK_GNU_MIRROR
 
-    $PXY_FRONTEND wget -O ../3rd/gcc-${PICK_GCC_REL}.tar.gz http://${USE_GNU_MIRROR}/gnu/gcc/gcc-${PICK_GCC_REL}/gcc-${PICK_GCC_REL}.tar.gz
+    $PXY_FRONTEND wget -O $WORKDIR/gcc-${PICK_GCC_REL}.tar.gz http://${USE_GNU_MIRROR}/gnu/gcc/gcc-${PICK_GCC_REL}/gcc-${PICK_GCC_REL}.tar.gz
     [[ $? -eq 0 ]] || exit 1
 
-    bash -c "cd ../3rd && tar xf gcc-${PICK_GCC_REL}.tar.gz"
+    bash -c "cd $WORKDIR && tar xf gcc-${PICK_GCC_REL}.tar.gz"
     [[ $? -eq 0 ]] || exit 1
 fi
 
 
-$PXY_FRONTEND bash -c "cd ../3rd/gcc-${PICK_GCC_REL} && ./contrib/download_prerequisites"
+$PXY_FRONTEND bash -c "cd $WORKDIR/gcc-${PICK_GCC_REL} && ./contrib/download_prerequisites"
 [[ $? -eq 0 ]] || exit 1
 
-bash -c "cd ../3rd/gcc-${PICK_GCC_REL} && mkdir -p objdir && cd objdir && ../configure --program-prefix='tgf-' --disable-multilib --disable-libsanitizer --enable-languages=c,c++ --enable-shared=libgcc,libstdc++"
+bash -c "cd $WORKDIR/gcc-${PICK_GCC_REL} && mkdir -p objdir && cd objdir && ../configure --program-prefix='tgf-' --disable-multilib --disable-libsanitizer --enable-languages=c,c++ --enable-shared=libgcc,libstdc++"
 [[ $? -eq 0 ]] || exit 1
 
-bash -c "cd ../3rd/gcc-${PICK_GCC_REL} && cd objdir && make -j$(nproc)"
+bash -c "cd $WORKDIR/gcc-${PICK_GCC_REL} && cd objdir && make -j$(nproc)"
 [[ $? -eq 0 ]] || exit 1
 
-sudo bash -c "cd ../3rd/gcc-${PICK_GCC_REL}/objdir && make install"
+sudo bash -c "cd $WORKDIR/gcc-${PICK_GCC_REL}/objdir && make install"
 [[ $? -eq 0 ]] || exit 1
 
 alt_ver_str=$(bash -c "tgf-gcc --version | head -1")
 [[ $? -eq 0 ]] || exit 1
 [[ $alt_ver_str =~ $PICK_GCC_REL ]] || exit 141
 
-sudo bash -c "rm -rf ../3rd/gcc-${PICK_GCC_REL}/objdir"
+sudo bash -c "rm -rf $WORKDIR/gcc-${PICK_GCC_REL}/objdir"
 [[ $? -eq 0 ]] || exit 1
 
 
